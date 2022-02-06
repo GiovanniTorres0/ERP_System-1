9feb9978ffec92d5d86239a03060dd6453974e7f
Marco Antonio Pereira <94496614+M4rcoToni@users.noreply.github.com>
2022-02-06 14:39:10 -0300

    comentarios

--- buscaPorId.java
+++ buscaPorId.java
@@ -1,24 +1,33 @@
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class buscaPorId {
     public static void ID() {
-        List<produto> lista = retornaLista.Lista();
-        
-		boolean avancar = false;
-        int id = 0;
+        List<produto> lista = retornaLista.Lista();         //crio a lista a partir da lista dos produtos
+        boolean avancar = true;
+        int id = 0 ;
         System.out.print("\nDigite o ID do produto: ");
-        id = new Scanner(System.in).nextInt();
+        try {
+            id = new Scanner(System.in).nextInt();          //guardo o que a pessoa digitou
+            avancar = false;
+        } catch (InputMismatchException m) {
+            System.out.println("Utilize numeros!");
+        }
+        if (!avancar) {                                    //inicia a pesquisa se estiver tudo certo
+            boolean encontrado = false;                    // variavel de controle
 
-        for (produto pro : lista) {
-            if (pro.getId() == id) {
-                System.out.println(pro);
-                avancar = true;
+            for (produto pro : lista) {                    // separo a lista por produtos
+                if (pro.getId() == id) {                   // comparo o id da que a pessoa digitou com o produto por meio do getId
+                    System.out.println(pro);
+                    encontrado = true;
+                }
+            }
+            if (encontrado == false) {                     // retorna mensagem caso nao ache o produto
+                System.out.println("Produto não Encontrado!");
             }
         }
-        if (avancar == false) {
-            System.out.println("Produto não Encontrado!");
-        }
+		
     }
 }
