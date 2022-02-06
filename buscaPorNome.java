982df31dd7be66e6bc06efdd98ba6eccf834c590
Marco Antonio Pereira <94496614+M4rcoToni@users.noreply.github.com>
2022-02-06 14:56:49 -0300

    comentario

--- buscaPorNome.java
+++ buscaPorNome.java
@@ -3,23 +3,22 @@ import java.util.List;
 import java.util.Scanner;
 public class buscaPorNome {
     public static void Nome() {
-        List<produto> lista = retornaLista.Lista();
+        List<produto> lista = retornaLista.Lista();             //crio a lista a partir da lista dos produtos
 
         boolean avancar = false;
-
         System.out.print("\nDigite o Nome do produto: ");
-        String nome = new Scanner(System.in).nextLine();
-
-        for (produto pro : lista) {
-            String[] listaNome = pro.getNome().split(" ");      // divide o nome no vetor
-            for (int j = 0; j < listaNome.length; j++) {
-                if (listaNome[j].regionMatches(0, nome, 0, nome.length())) {    //pesquisa em cada parte do nome do produto 
-                    System.out.println(pro);
+        String nome = new Scanner(System.in).nextLine();        //guardo o que a pessoa digitou
+        int i ;
+        for (produto pro : lista) {                             //separo a lista por produtos
+            String[] listaNome = pro.getNome().split(" ");      //divide o nome pelos espaçoes e coloca no vetor
+            for (i = 0; i < listaNome.length; i++) {            //for depende de quantas partes o nome foi dividido
+                if (listaNome[i].regionMatches(0, nome, 0, nome.length())) {    //compara a resposta com cada parte do nome do produto
+                    System.out.println(pro);                    //imprime o produto de acordo com o toString
                     avancar = true;
                 }
             }
         }
-        if (avancar == false) {
+        if (!avancar ) {                                 //pq um if em vez de else na linha 19 ? porque cada vez que ele nao encontra ira imprimir
             System.out.println("Produto não Encontrado!");
         }
     }
