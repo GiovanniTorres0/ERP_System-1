3cf46e0949be46fca838e5fe09255de82520f4bf
Marco Antonio Pereira <94496614+M4rcoToni@users.noreply.github.com>
2022-02-06 14:33:10 -0300

    comentarios

--- retornaLista.java
+++ retornaLista.java
@@ -7,25 +7,25 @@ import java.util.ArrayList;
 public class retornaLista {
     public static List<produto> Lista() {
 		List<produto> lista = new ArrayList<produto>();
-		String arquivo = "C:/Users/usuario_2.UTEC-7314/Desktop/ERP/Products.csv";
+		String arquivo = "C:/Users/usuario_2.UTEC-7314/Desktop/ERP/Products.csv"; 
 
-		try (BufferedReader buf = new BufferedReader(new FileReader(arquivo))) {
-			String linha = buf.readLine();
-			linha = buf.readLine();
-			for(int i = 0; i <= 100; i++){	//alterar o tamanho
-				String[]produtos = linha.split(",");		 //separo o arquivo pelas virgulas colocando em vetor
+		try (BufferedReader buf = new BufferedReader(new FileReader(arquivo))) { 	//leitura mais otimizada | FileReader classe de leitura de dados  | BufferedReader gerencia a memoria usada para a leitura
+			String linha = buf.readLine();								//readLine le a linha do arquivo e retorna uma String
+			linha = buf.readLine(); 									//pula a primeria linha que é cabeçalho
+			for(int i = 0; i <= 100; i++){								//alterar o tamanho e muda pra while(linha != null){ se nao tiver mais linhas pra ler ele retorna nulo
+				String[]produtos = linha.split(",");					//le a linha separando o arquivo pelas virgulas e colocando no vetor
 				int id = Integer.parseInt(produtos[0]);
 				Double preço = Double.parseDouble(produtos[2]);
 				String nome =produtos[21];
-				produto prod = new produto(id, preço, nome); //passa como parementro as info do vetor pra classe
-				lista.add(prod); 		//cria um produto com os dados do vetor e coloco na lista
+				produto prod = new produto(id, preço, nome); 			//passa como parementro as info do vetor instanciando o produto
+				lista.add(prod); 										//preenche a lista com o produto
 				
-				linha = buf.readLine();
+				linha = buf.readLine();									//le a proxima linha do arquivo
 			}	
 		}
 		catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
-			return lista;
+			return lista;		//lista diponivel para usar de qualquer lugar
 	}
 }
